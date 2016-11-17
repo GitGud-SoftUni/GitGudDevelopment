a201c1426d64472739e7b689158e3f23ebe21072
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2016-11-17 14:35:57 +0200

    upload is uploading in directory no error this time

--- GitGud/src/GitGud/Services/DebugUploadService.cs
+++ GitGud/src/GitGud/Services/DebugUploadService.cs
@@ -18,7 +18,7 @@ namespace GitGud.Services
             _environment = environemnt;
         }
 
-        public async void UploadSong(IFormFile file, string songName, string artistName, string[] tags)
+        public void UploadSong(IFormFile file, string songName, string artistName, string[] tags)
         {
             //TODO change stuff so it has user Id or something and Upload it to Database ?? Profit ??
             var uploads = Path.Combine(_environment.WebRootPath, "uploads");
@@ -27,7 +27,7 @@ namespace GitGud.Services
                 {
                     using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
                     {
-                        await file.CopyToAsync(fileStream);
+                        file.CopyTo(fileStream);
                     }
                 }
 
