39a2dbba48f7e5b47e317c2a16c06943fcb71eb4
marialbst <marialbst@gmail.com>
2016-11-17 21:18:13 +0200

    empty admin controller added

---
+++ GitGud/src/GitGud/Controllers/Web/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitGud.Controllers.Web
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        //here to add everything that an admin can do
+        //create categories, manage users etc.
+        //first to find out how a user can take role=admin???
+    }
+}
--- GitGud/src/GitGud/Models/User.cs
+++ GitGud/src/GitGud/Models/User.cs
@@ -10,12 +10,14 @@ namespace GitGud.Models
     public class User : IdentityUser
     {
         //Identity package contains every property for a user...
-//        public int Id { get; set; }
-//        public string Username { get; set; } //a.k.a email
-//        public string FullName { get; set; }
-//        public string PasswordHash { get; set; }// a.k.a encrypted password string which is stored in db instead password 
-//        public string Role { get; set; } // user or admin 
-//        public string Salt { get; set; }//a.k.a. encryption key for the password before storing in db
-        public ICollection<Song> Songs { get; set; }
+        //        public int Id { get; set; }
+        //        public string Username { get; set; } //a.k.a email
+        //        public string FullName { get; set; }
+        //        public string PasswordHash { get; set; }// a.k.a encrypted password string which is stored in db instead password 
+        //        public string Role { get; set; } // user or admin 
+        //        public string Salt { get; set; }//a.k.a. encryption key for the password before storing in db
+
+
+        //public ICollection<Song> Songs { get; set; }
     }
 }
