8ef4b901a7458a3f57eb3eef32304a60b35b63fe
marialbst <marialbst@gmail.com>
2016-11-19 15:39:56 +0200

    changed min length of password

--- GitGud/src/GitGud/wwwroot/js/script.js
+++ GitGud/src/GitGud/wwwroot/js/script.js
@@ -4,7 +4,6 @@
     // UPLOAD CLASS DEFINITION
     // ======================
 
-    var dropZone = document.getElementById('drop-zone');
     var uploadForm = document.getElementById('js-upload-form');
 
     var startUpload = function(files) {
@@ -17,22 +16,5 @@
 
         startUpload(uploadFiles)
     })
-
-    dropZone.ondrop = function(e) {
-        e.preventDefault();
-        this.className = 'upload-drop-zone';
-
-        startUpload(e.dataTransfer.files)
-    }
-
-    dropZone.ondragover = function() {
-        this.className = 'upload-drop-zone drop';
-        return false;
-    }
-
-    dropZone.ondragleave = function() {
-        this.className = 'upload-drop-zone';
-        return false;
-    }
 
 }(jQuery);
